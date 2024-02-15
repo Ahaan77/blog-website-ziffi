61733aa9145127cd189c220b46befa1b1bf30d4e
AhaanSehgal <ahaan@threely.io>
2024-02-15 23:09:05 +0530

    Fix cors

--- dist/utils/connection.js
+++ dist/utils/connection.js
@@ -2,6 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.connectToDatabase = void 0;
 const mongoose_1 = require("mongoose");
+//Establish connection with mongo
 const connectToDatabase = async () => {
     try {
         await (0, mongoose_1.connect)(`mongodb+srv://admin:KonIpXBsEZPwyGP8@cluster0.mmh7kh7.mongodb.net/?retryWrites=true&w=majority`);
--- utils/connection.ts
+++ utils/connection.ts
@@ -1,5 +1,6 @@
 import { connect } from "mongoose";
 
+//Establish connection with mongo
 export const connectToDatabase = async () => {
     try {
         await connect(`mongodb+srv://admin:KonIpXBsEZPwyGP8@cluster0.mmh7kh7.mongodb.net/?retryWrites=true&w=majority`)
